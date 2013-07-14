a2609aae56abdf6f855b076b13eb1f7cb7311305
Shawn Presser <shawnpresser@gmail.com>
2013-07-14 23:48:35 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var s = fs.readFileSync('index.html', { encoding: 'utf-8' });
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
